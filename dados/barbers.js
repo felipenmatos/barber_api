f2a7546fea3bda3d8ebd3aa76a8013a5b9de2bf0
felipenmatos <notfelipe2@hotmail.com>
2022-10-16 21:14:54 -0300

    feat: creation of initial data for barbers listing

---
+++ dados/barbers.js
@@ -0,0 +1,12 @@
+const barbers = [
+  {
+    nome: "Felipe",
+    idade: 28,
+    especialidade: "Cabelo/Barba",
+  },
+  {
+    nome: "João",
+    idade: 35,
+    especialidade: "Cabelo/Barba/Sobrancelha",
+  },
+];
