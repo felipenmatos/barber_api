228ab66e9e0e2acf5f3fbfacd6669364edbc3307
felipenmatos <notfelipe2@hotmail.com>
2022-11-09 09:58:00 -0300

    feat: implementation of validation treatment in the barber register

--- controlador/controlador.js
+++ controlador/controlador.js
@@ -1,6 +1,6 @@
 const barbers = require("../dados/barbers");
 
-const especialidade = [
+const especialidadeValida = [
   "Cabelo",
   "Barba",
   "Sobrancelha",
@@ -26,9 +26,55 @@ function consultarId(req, res) {
   res.json(barber);
 }
 
+function validateRegistration(register) {
+  if (!register.nome) {
+    return "O campo nome é obrigatório";
+  }
+
+  if (!register.idade) {
+    return "O campo idade é obrigatório";
+  }
+
+  if (typeof register.nome !== "string") {
+    return "O campo nome deve ser preenchido como um texto";
+  }
+
+  if (!register.idade) {
+    return "O campo idade é obrigatório";
+  }
+
+  if (register.idade < 18) {
+    return "O barbeiro deve ser maior de idade";
+  }
+
+  if (typeof register.idade !== "number") {
+    return "O campo idade deve ser preenchido como um texto";
+  }
+
+  if (!register.especialidade) {
+    return "O campo especialidade é obrigatório";
+  }
+
+  if (typeof register.especialidade !== "string") {
+    return "O campo especialidade deve ser preenchido como um texto";
+  }
+
+  if (!especialidadeValida.includes(register.especialidade)) {
+    return "Área de especialidade informada é invalida.";
+  }
+}
+
 let proximoId = 3;
 
 function createBarber(req, res) {
+  const erro = validateRegistration(req.body);
+
+  if (erro) {
+    res.status(400);
+    res.json({ erro });
+    return;
+  }
+
   const newBarber = {
     id: proximoId,
     nome: req.body.nome,
